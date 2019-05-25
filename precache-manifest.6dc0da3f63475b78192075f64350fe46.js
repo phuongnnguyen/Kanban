8187577f3ffcbe6107686c8a38f570a79819d3c9
phuongnnguyen <46915743+phuongnnguyen@users.noreply.github.com>
2019-05-25 15:48:14 +0700

    Updates

---
+++ precache-manifest.6dc0da3f63475b78192075f64350fe46.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3a80d6df6397191c2a183ffe8eaed3",
+    "url": "/Kanban/index.html"
+  },
+  {
+    "revision": "bc36911d758270bad0ef",
+    "url": "/Kanban/static/css/main.f8b7dd68.chunk.css"
+  },
+  {
+    "revision": "5c9ff16ec5bee154aee2",
+    "url": "/Kanban/static/js/2.5286435d.chunk.js"
+  },
+  {
+    "revision": "bc36911d758270bad0ef",
+    "url": "/Kanban/static/js/main.ea56ec99.chunk.js"
+  },
+  {
+    "revision": "958ccfa236bbc25c9ab0",
+    "url": "/Kanban/static/js/runtime~main.619b7780.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Kanban/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Kanban/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Kanban/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Kanban/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Kanban/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
